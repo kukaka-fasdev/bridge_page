1382ea7915f4394a3a5f7ab8e77c85df55bff377
kukaka <hykim@fashionandstyle.com>
2024-10-08 18:33:21 +0900

    INIT

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import BridgePage from './bridge/BridgePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+      <div className="App">
+        {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +18,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+      </header> */}
+        <BridgePage/>
+      </div>
   );
 }
 
---
+++ src/bridge/BridgePage.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from "react";
+
+function BridgePage() {
+
+    useEffect(() => {
+        document.title = "패션&스타일";
+    }, []);
+
+    useEffect(() => {
+        const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+        if (/android/i.test(userAgent)) {
+            // Android 사용자일 경우 딥링크를 시도하고, 앱 스토어로 리다이렉트
+            const iframe = document.createElement("iframe");
+            iframe.style.display = "none";
+            //   iframe.src = "fashionandstyle://";
+            iframe.src = "https://www.fashionandstyle.com";
+            document.body.appendChild(iframe);
+            setTimeout(() => {
+                window.location = "https://play.google.com/store/apps/details?id=com.fas.android";
+            }, 2000);
+        } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            // iOS 사용자일 경우 딥링크를 시도하고, 앱 스토어로 리다이렉트
+            const iframe = document.createElement("iframe");
+            iframe.style.display = "none";
+            //   iframe.src = "fashionandstyle://";
+            iframe.src = "https://www.fashionandstyle.com";
+            document.body.appendChild(iframe);
+            setTimeout(() => {
+                window.location = "https://apps.apple.com/app/id1620312420";
+            }, 2000);
+        }
+    }, []);
+
+    return(
+        <>
+            <div>
+                <h1>
+                    브릿지 페이지 입니다.
+                </h1>
+                <p>잠시 후 앱으로 이동합니다. 이동되지 않으면 앱 스토어에서 설치해주세요.</p>
+                <div>
+                    <a href="https://www.fashionandstyle.com">앱에서 열기</a>
+                    <p>또는</p>
+                    <a href="https://apps.apple.com/app/id1620312420">앱스토어에서 설치</a>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default BridgePage;
