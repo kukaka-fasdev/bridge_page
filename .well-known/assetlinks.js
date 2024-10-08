ab0c51e604a34ea05f5a40709c441882b2bc368b
kukaka <hykim@fashionandstyle.com>
2024-10-08 19:45:00 +0900

    [FEAT] 딥링크 테스트 - HYKIM 2024/10/0

---
+++ .well-known/assetlinks.js
@@ -0,0 +1,14 @@
+[
+    {
+        "relation": [
+            "delegate_permission/common.handle_all_urls"
+        ],
+        "target": {
+            "namespace": "android_app",
+            "package_name": "com.fas.android",
+            "sha256_cert_fingerprints": [
+                "74:65:81:3B:8F:76:BE:24:27:19:2C:52:0C:4A:F2:6B:24:4E:A9:DD:DA:51:75:C2:9E:D8:39:9D:0D:B6:C8:66"
+            ]
+        }
+    }
+]
