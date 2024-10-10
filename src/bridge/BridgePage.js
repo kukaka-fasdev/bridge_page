b06032b7de92ababdf5d469ee0c598c8922a8031
kukaka <hykim@fashionandstyle.com>
2024-10-10 14:17:42 +0900

    [FEAT] 딥링크 테스트 - HYKIM 2024/10/08

--- src/bridge/BridgePage.js
+++ src/bridge/BridgePage.js
@@ -27,7 +27,7 @@ function BridgePage() {
             const url = window.location.href.replace("https://www.fashionandstyle.com", "fashionandstyle://")
 
             alert(url);
-            window.location = url;
+            window.location = "https://www.fashionandstyle.com";
 
             setTimeout(function () {
                 // wait a second and then to go App Store or your Website
