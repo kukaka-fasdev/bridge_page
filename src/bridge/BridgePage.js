d508107ee9fba77e7a52b02595cb76b3db975043
kukaka <hykim@fashionandstyle.com>
2024-10-10 13:17:13 +0900

    [FEAT] 딥링크 테스트 - HYKIM 2024/10/08

--- src/bridge/BridgePage.js
+++ src/bridge/BridgePage.js
@@ -22,51 +22,51 @@ function BridgePage() {
             }, 2000);
         } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
             // iOS 사용자일 경우 딥링크를 시도하고, 앱 스토어로 리다이렉트
-            const iframe = document.createElement("iframe");
-            iframe.style.display = "none";
-            //iframe.src = "fashionandstyle://";
-            iframe.src = "https://www.fashionandstyle.com";
-            document.body.appendChild(iframe);
-            window.setTimeout(() => {
-                document.body.removeChild(iframe);
-            }, 1000);
-
-            alert(1);
-            window.open("https://www.fashionandstyle.com", "_blank");
-
-            alert(2);
-            window.open('fashionandstyle://', "_blank");
-
-            alert(3);
-            window.location.replace('fashionandstyle://');
-
-            alert(4);
-            window.location.replace('https://www.fashionandstyle.com');
-
-            alert(5);
-            window.location.href = "fashionandstyle://";
-
-            alert(6);
-            window.location.href = "https://www.fashionandstyle.com";
-
-            alert(7);
-            // const link = document.getElementById("moveToAPP");
-            // alert(link.href);
-            // link.click();
-
-            const link = document.createElement('a');
-            link.href = 'https://www.fashionandstyle.com';
-            link.target = '_blank';
-            link.rel = 'noreferrer noopener';
-
-            link.text = '새로운 탭에서 열기';
-
-            document.body.appendChild(link);
-
-            // 클릭 이벤트 추가 (선택 사항)
-            link.addEventListener('click', () => {
-                // 클릭 시 실행될 추가적인 로직
-            });
+            // const iframe = document.createElement("iframe");
+            // iframe.style.display = "none";
+            // //iframe.src = "fashionandstyle://";
+            // iframe.src = "https://www.fashionandstyle.com";
+            // document.body.appendChild(iframe);
+            // window.setTimeout(() => {
+            //     document.body.removeChild(iframe);
+            // }, 1000);
+            //
+            // alert(1);
+            // window.open("https://www.fashionandstyle.com", "_blank");
+            //
+            // alert(2);
+            // window.open('fashionandstyle://', "_blank");
+            //
+            // alert(3);
+            // window.location.replace('fashionandstyle://');
+            //
+            // alert(4);
+            // window.location.replace('https://www.fashionandstyle.com');
+            //
+            // alert(5);
+            // window.location.href = "fashionandstyle://";
+            //
+            // alert(6);
+            // window.location.href = "https://www.fashionandstyle.com";
+            //
+            // alert(7);
+            // // const link = document.getElementById("moveToAPP");
+            // // alert(link.href);
+            // // link.click();
+            //
+            // const link = document.createElement('a');
+            // link.href = 'https://www.fashionandstyle.com';
+            // link.target = '_blank';
+            // link.rel = 'noreferrer noopener';
+            //
+            // link.text = '새로운 탭에서 열기';
+            //
+            // document.body.appendChild(link);
+            //
+            // // 클릭 이벤트 추가 (선택 사항)
+            // link.addEventListener('click', () => {
+            //     // 클릭 시 실행될 추가적인 로직
+            // });
 
             // setTimeout(() => {
             //     alert(10);
@@ -92,6 +92,8 @@ function BridgePage() {
             link.text = '새로운 탭에서 열기';
 
             document.body.appendChild(link);
+
+            link.click();
 
         };
 
