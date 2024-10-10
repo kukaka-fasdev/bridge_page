784d69b527be0b54e89aff205322083b3526c5a4
kukaka <hykim@fashionandstyle.com>
2024-10-10 14:07:25 +0900

    [FEAT] 딥링크 테스트 - HYKIM 2024/10/08

--- src/bridge/BridgePage.js
+++ src/bridge/BridgePage.js
@@ -21,6 +21,19 @@ function BridgePage() {
                 window.location = "https://play.google.com/store/apps/details?id=com.fas.android";
             }, 2000);
         } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+
+            const isUserComingFromInstagramReferral = document.referrer.includes("instagram");
+            if (isUserComingFromInstagramReferral) {
+                const url = window.location.href.replace("https://www.fashionandstyle.com", "fashionandstyle://")
+                window.location = url;
+
+                setTimeout(function () {
+                    // wait a second and then to go App Store or your Website
+                    // if the user hasn't installed yet.
+                    window.location = "https://apps.apple.com/app/id1620312420";
+                }, 1000);
+            }
+
             // iOS 사용자일 경우 딥링크를 시도하고, 앱 스토어로 리다이렉트
             // const iframe = document.createElement("iframe");
             // iframe.style.display = "none";
@@ -95,24 +108,24 @@ function BridgePage() {
             //
             // link.click();
 
-            const isUserComingFromInstagramReferral = document.referrer.includes("instagram");
-            if (isUserComingFromInstagramReferral) {
-                const url = window.location.href.replace("https://www.fashionandstyle.com", "fashionandstyle://")
-                window.location = url;
-
-                setTimeout(function () {
-                    // wait a second and then to go App Store or your Website
-                    // if the user hasn't installed yet.
-                    window.location = "https://apps.apple.com/app/id1620312420";
-                }, 1000);
-            }
+            // const isUserComingFromInstagramReferral = document.referrer.includes("instagram");
+            // if (isUserComingFromInstagramReferral) {
+            //     const url = window.location.href.replace("https://www.fashionandstyle.com", "fashionandstyle://")
+            //     window.location = url;
+            //
+            //     setTimeout(function () {
+            //         // wait a second and then to go App Store or your Website
+            //         // if the user hasn't installed yet.
+            //         window.location = "https://apps.apple.com/app/id1620312420";
+            //     }, 1000);
+            // }
 
         };
 
-        window.addEventListener('load', handleLoad);
-
-        // Clean-up 함수: 컴포넌트가 언마운트될 때 이벤트 리스너를 제거
-        return () => window.removeEventListener('load', handleLoad);
+        // window.addEventListener('load', handleLoad);
+        //
+        // // Clean-up 함수: 컴포넌트가 언마운트될 때 이벤트 리스너를 제거
+        // return () => window.removeEventListener('load', handleLoad);
 
     }, []);
 
